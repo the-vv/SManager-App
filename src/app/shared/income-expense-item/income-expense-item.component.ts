ff24a1fc61318928737fa18886ceabf632e54257
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2022-08-08 23:15:01 +0530

    small ui changes

--- src/app/shared/income-expense-item/income-expense-item.component.ts
+++ src/app/shared/income-expense-item/income-expense-item.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { ECashType, IIncomeExpense } from 'src/app/models/common';
 import { CommonService } from 'src/app/services/common.service';
+import { ConfigService } from 'src/app/services/config.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { CreateSharedComponent } from '../create-shared/create-shared.component';
 
@@ -15,16 +16,20 @@ export class IncomeExpenseItemComponent implements OnInit {
 
   @Input() item: IIncomeExpense;
   @Input() categoryName: string;
+  public currentBankName: string;
 
   eCashType = ECashType;
   constructor(
     private common: CommonService,
     private firebase: FirebaseService,
     private modalController: ModalController,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private config: ConfigService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.currentBankName = this.config.currentUserAccounts.find(el => el.id === this.item.accountId)?.name;
+  }
 
   async deleteItem() {
     if (await this.common.showDeleteConfrmation(this.item.title)) {
@@ -53,11 +58,12 @@ export class IncomeExpenseItemComponent implements OnInit {
       header: this.item.title,
       message: `<p class='text-${this.item.type === ECashType.expense ? 'danger' : 'success'} font-bold p-0 m-0 h5'>
         ${this.item.type.charAt(0).toUpperCase() + this.item.type.slice(1)}: ₹ ${this.item.amount}</p>
-        <div class='mt-1 block'><strong class=''>Category: </strong> ${this.categoryName}</div>
-        <div class='mt-1 block'><strong class=''>Date:</strong>
+        <div class='mt-0.5 block'><strong class=''>Category: </strong> ${this.categoryName}</div>
+        <div class='mt-0.5 block'><strong class=''>Date:</strong>
             ${new DatePipe('en').transform(this.item.datetime, 'dd/M/yyyy hh:mm a')}
           </div>
-        <div class='mt-1 block'><strong class=''>Description: </strong> ${this.item.description}</div>
+        <div class='mt-0.5 block'><strong class=''>Account:</strong> ${this.currentBankName}</div>
+        <div class='mt-0.5 block'><strong class=''>Description: </strong> ${this.item.description}</div>
       `,
       buttons: [{ text: 'Close' }]
     }).then(alert => alert.present());
