f51b786a8230e04d3d5a1012c155dc1223e7f221
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2022-08-08 23:42:46 +0530

    pie chart according to category

--- src/app/dashboard/overview/overview.page.ts
+++ src/app/dashboard/overview/overview.page.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Subscription } from 'rxjs';
-import { IAccount } from 'src/app/models/common';
+import { Subscription, take } from 'rxjs';
+import { IAccount, ICategory } from 'src/app/models/common';
 import { CashService } from 'src/app/services/cash.service';
+import { CommonService } from 'src/app/services/common.service';
 import { ConfigService } from 'src/app/services/config.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 Chart.register(...registerables);
@@ -19,13 +20,16 @@ export class OverviewPage implements OnInit {
   @ViewChild('lineCanvas') lineCanvas: ElementRef;
   chartRenders: Chart[] = [];
   subs: Subscription;
+  subs1: Subscription;
   allAccounts: IAccount[] = [];
   currentAccount: IAccount;
+  allCategories: ICategory[] = [];
 
   constructor(
     public cashService: CashService,
     private firebase: FirebaseService,
-    private config: ConfigService
+    private config: ConfigService,
+    private common: CommonService
   ) { }
 
   ngOnInit() { }
@@ -49,85 +53,110 @@ export class OverviewPage implements OnInit {
           el.destroy();
         });
       }
-      this.chartRenders.push(
-        new Chart(this.pieCanvas.nativeElement, {
-          plugins: [ChartDataLabels],
-          type: 'pie',
-          data: {
-            labels: ['Income', 'Expense'],
-            datasets: [
-              {
-                label: 'Overview',
-                data: [this.cashService.currentMonthData.totalIncome, this.cashService.currentMonthData.totalExpense],
-                backgroundColor: [
-                  'rgba(0, 255, 0, 0.5)',
-                  'rgba(255, 0, 0, 0.5)'
-                ],
-                hoverBackgroundColor: [
-                  'rgba(0, 255, 0, 0.7)',
-                  'rgba(255, 0, 0, 0.7)'
-                ],
-              }
-            ]
-          },
-          options: {
-            responsive: true,
-            plugins: {
-              datalabels: {
-                formatter: (val) => `₹${val}`,
-                labels: {
-                  title: {
-                    font: {
-                      weight: 'bold',
-                    },
-                    color: 'white'
+      const groupedExpenses = this.groupBy(this.cashService.allExpenses, 'categoryId');
+      for (const key in groupedExpenses) {
+        if (groupedExpenses.hasOwnProperty(key)) {
+          groupedExpenses[key] = groupedExpenses[key].reduce((acc, cur) => acc + cur.amount, 0);
+        }
+      }
+      this.firebase.getAllUserCategories().pipe(take(1)).subscribe({
+        next: (categories) => {
+          this.allCategories = categories;
+          const expenses = [];
+          for (const key in groupedExpenses) {
+            if (groupedExpenses.hasOwnProperty(key)) {
+              expenses.push({
+                name: this.allCategories.find(el => el.id === key)?.name ?? 'Uncategorized',
+                value: groupedExpenses[key]
+              });
+            }
+          }
+          this.chartRenders.push(
+            new Chart(this.pieCanvas.nativeElement, {
+              plugins: [ChartDataLabels],
+              type: 'pie',
+              data: {
+                labels: expenses.map(el => el.name),
+                datasets: [
+                  {
+                    label: 'Overview',
+                    data: expenses.map(el => el.value),
+                    backgroundColor: [
+                      'rgba(0, 0, 255, 0.5)',
+                      'rgba(255, 0, 0, 0.5)',
+                      'rgba(0, 255, 0.5)'
+                    ],
+                    hoverBackgroundColor: [
+                      'rgba(0, 255, 0, 0.7)',
+                      'rgba(255, 0, 0, 0.7)',
+                      'rgba(0, 255, 0.5)'
+                    ],
+                  }
+                ]
+              },
+              options: {
+                responsive: true,
+                plugins: {
+                  datalabels: {
+                    formatter: (val) => `₹${val}`,
+                    labels: {
+                      title: {
+                        font: {
+                          weight: 'bold',
+                        },
+                        color: 'white'
+                      }
+                    }
                   }
                 }
               }
-            }
-          }
-        })
-      );
-      const now = new Date();
-      const currentDays = Array(new Date(now.getFullYear(), now.getMonth() + 1, now.getDate()).getDate()).fill(0).map((_, i) => i + 1);
-      const currentIncomes = currentDays.map(day => {
-        const income = this.cashService.allIncomes
-          .filter(i => (i.datetime as Date).getDate() === day)
-          .reduce((acc, curr) => acc + curr.amount, 0);
-        return income ? income : 0;
-      });
-      const currentExpenses = currentDays.map(day => {
-        const expense = this.cashService.allExpenses.filter(i => (i.datetime as Date).getDate() === day)
-          .reduce((acc, curr) => acc + curr.amount, 0);
-        return expense ? expense : 0;
-      });
-      this.chartRenders.push(
-        new Chart(this.lineCanvas.nativeElement, {
-          // plugins: [ChartDataLabels],
-          type: 'line',
-          data: {
-            // eslint-disable-next-line max-len
-            labels: currentDays, // total days in current month
-            datasets: [
-              {
-                label: 'Income',
-                data: currentIncomes,
-                borderColor: 'rgba(0, 255, 0, 0.5)',
-                backgroundColor: 'rgba(0, 255, 0, 0.5)',
+            })
+          );
+          const now = new Date();
+          const currentDays = Array(new Date(now.getFullYear(), now.getMonth() + 1, now.getDate()).getDate()).fill(0).map((_, i) => i + 1);
+          const currentIncomes = currentDays.map(day => {
+            const income = this.cashService.allIncomes
+              .filter(i => (i.datetime as Date).getDate() === day)
+              .reduce((acc, curr) => acc + curr.amount, 0);
+            return income ? income : 0;
+          });
+          const currentExpenses = currentDays.map(day => {
+            const expense = this.cashService.allExpenses.filter(i => (i.datetime as Date).getDate() === day)
+              .reduce((acc, curr) => acc + curr.amount, 0);
+            return expense ? expense : 0;
+          });
+          this.chartRenders.push(
+            new Chart(this.lineCanvas.nativeElement, {
+              // plugins: [ChartDataLabels],
+              type: 'line',
+              data: {
+                // eslint-disable-next-line max-len
+                labels: currentDays, // total days in current month
+                datasets: [
+                  {
+                    label: 'Income',
+                    data: currentIncomes,
+                    borderColor: 'rgba(0, 255, 0, 0.5)',
+                    backgroundColor: 'rgba(0, 255, 0, 0.5)',
+                  },
+                  {
+                    label: 'Expense',
+                    data: currentExpenses,
+                    borderColor: 'rgba(255, 0, 0, 0.5)',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                  }
+                ]
               },
-              {
-                label: 'Expense',
-                data: currentExpenses,
-                borderColor: 'rgba(255, 0, 0, 0.5)',
-                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              options: {
+                responsive: true,
               }
-            ]
-          },
-          options: {
-            responsive: true,
-          }
-        })
-      );
+            })
+          );
+        },
+        error: () => {
+          this.common.showToast('Something went wrong');
+        }
+      });
     });
   }
   ionViewDidLeave() {
@@ -135,4 +164,12 @@ export class OverviewPage implements OnInit {
       el.destroy();
     });
   }
+
+  public groupBy(xs: any, key: string) {
+    return xs.reduce((rv: any, x: any) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
 }
